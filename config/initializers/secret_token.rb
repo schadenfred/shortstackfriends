1f7b4e9d7f7ebb598bf172588d54705a1812ac2e
Fred Schoeneman <fred.schoeneman@gmail.com>
2013-03-04 17:04:21 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shortstackfriends::Application.config.secret_token = 'a225e66dfd128edd336943151ad37dcdcd3fbacaa86a32ff951ef7f740e0f5a9ad67c2d6c91d71ec1181f25ee2193ac691cb8417707b68f803c6a0c30ad2b25c'
